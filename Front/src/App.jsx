e26d4109530658878e65f4f4d37bb2220bb5499c
Amir <vibelessyoung@gmail.com>
2026-02-21 09:00:47 +0330

    start create register and login and routing

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,3 +1,5 @@
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 import Navbar from "./components/Navbar";
 import "./styles/Style.css";
 import {
@@ -10,9 +12,10 @@ import {
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path="/" element={""}></Route>
+        <Route path="/" element={<Navbar />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
