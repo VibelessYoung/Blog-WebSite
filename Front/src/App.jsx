5eb8cf085ea0d195e192e08ce22b49ded4a5750b
Amir <vibelessyoung@gmail.com>
2026-02-27 13:47:09 +0330

    add image to app(main page

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -4,18 +4,27 @@ import Register from "./components/auth/Register";
 import Navbar from "./components/Navbar";
 import "./styles/Style.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import bgImageMain from "./assets/Images/bg-l.jpg";
 
 axios.defaults.baseURL = "http://localhost:8000";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/Register" element={<Register />} />
-        <Route path="/Login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <div
+      className="relative min-h-screen bg-cover bg-center"
+      style={{ backgroundImage: `url(${bgImageMain})` }}
+    >
+      <div className="absolute inset-0 bg-black/60"></div>
+      <BrowserRouter>
+        <div className="relative z-10">
+          <Navbar />
+          <Routes>
+            <Route path="/Register" element={<Register />} />
+            <Route path="/Login" element={<Login />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
